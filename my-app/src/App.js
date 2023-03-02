0b78c801ba2dd1b8c1d75205e9abc36f0afa6383
JureSeselj <jure.seselj@gmail.com>
2023-03-02 21:11:43 +0000

    Removed Content.js import from App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,6 @@ import css from './App.module.css';
 import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
 import ContentHooks from './components/ContentHooks';
 
 function App() {
